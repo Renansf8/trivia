3a5826f53a0554483e2ced24124e1770e30ddd28
Renan Ferreira <renansantiago2013@hotmail.com>
2021-10-03 22:15:10 -0300

    Inicia implementacao dos testes

--- src/test/Home.spec.js
+++ src/test/Home.spec.js
@@ -2,21 +2,11 @@ import React from 'react';
 import Home from '../pages/Home';
 import { screen } from '@testing-library/react';
 import renderWithRouter from '../helpers/renderWithRouter';
-import userEvent from '@testing-library/user-event';
 
 describe('Testa a página Home', () => {
   it('Testa se a página Home é renderizada em seu estado inicial', () => {
     renderWithRouter(<Home />);
     const title = screen.getByText(/welcome to trivia/i);
     expect(title).toBeInTheDocument;
-  });
-
-  it('Testa se a página é redirecioanada para a página start', () => {
-    renderWithRouter(<Home />);
-    const startLink = screen.getByRole('link', { name: '1' });
-    userEvent.click(startLink);
-
-    const title = screen.getByText(/question/i);
-    expect(title).toBeDefined();
   });
 });
