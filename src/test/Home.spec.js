0bd7f43448ead30fd964cd06fd9d19e220fd1ca6
Renan Ferreira <renansantiago2013@hotmail.com>
2021-10-11 20:00:19 -0300

    Adiciona mais um teste na pagina Home

--- src/test/Home.spec.js
+++ src/test/Home.spec.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import Home from '../pages/Home';
 import { screen } from '@testing-library/react';
-import renderWithRouter from '../helpers/renderWithRouter';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
 
 describe('Testa a página Home', () => {
   it('Testa se a página Home é renderizada em seu estado inicial', () => {
     renderWithRouter(<Home />);
     const title = screen.getByText(/welcome to trivia/i);
-    expect(title).toBeDefined();
+    expect(title).toBeInTheDocument();
 
-    const button1 = screen.getByRole('button', { name: '1' });
-    expect(button1).toBeDefined();
+    const button1 = screen.getByRole('link', { name: '1' });
+    expect(button1).toBeInTheDocument();
+  });
+
+  it('Testa se a página contém 5 botões', () => {
+    renderWithRouter(<Home />);
+    const allButtons = screen.getAllByRole('button');
+    expect(allButtons).toHaveLength(5);
   });
 
   it('Testa a rota da página Start', () => {
     const { history } = renderWithRouter(<Home />);
 
-    history.push('/start');
-    const pathName = history.location.pathname;
-    expect(pathName).toBe('/start');
+    const button1 = screen.getByRole('link', { name: '1' });
+    userEvent.click(button1);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/start');
   });
 });
